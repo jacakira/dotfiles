f977e3bcd807af69e772fb8be74635c18d9769a1
jacakira <tanase.akira@gmail.com>
2024-03-01 15:06:43 +0000

    added starship, nvim and zathura

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "rose-pine/neovim",
+    styles = {
+      italic = false,
+    },
+  },
+  --  { "rebelot/kanagawa.nvim" },
+  --  { "EdenEast/nightfox.nvim" },
+  --  { "shaunsingh/nord.nvim" },
+  --  { "kepano/flexoki" },
+  --  { "nyoom-engineering/nyoom.nvim" },
+  --  { "savq/melange-nvim" },
+  --  { "Shatur/neovim-ayu" },
+  --  { "catppuccin/nvim" },
+  --  { "ful1e5/onedark.nvim" },
+  --  { "NLKNguyen/papercolor-theme" },
+  --{
+  --"LazyVim/LazyVim",
+  --opts = {
+  --colorscheme = "rose-pine-moon",
+  --},
+  --},
+}
