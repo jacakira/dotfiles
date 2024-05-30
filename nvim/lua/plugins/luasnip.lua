d993a68d001d8a39c993a577c8cf579a813862dd
jacakira <tanase.akira@gmail.com>
2024-05-31 00:49:19 +0100

    cleaned up

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -1,19 +1,16 @@
 return {
   "L3MON4D3/LuaSnip",
-  config = function()
-    require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/plugins/luasnip/" })
-    vim.cmd([[
-    " Use Tab to expand and jump through snippets
-    imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
-    smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
-    " Use Shift-Tab to jump backwards through snippets
-    imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
-    smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
-    ]])
-  end,
+  dependencies = {
+    require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/plugins/luasnip/" }),
+  },
   opts = {
+    history = false,
     enable_autosnippets = true,
-    update_events = "TextChanged, TextChangedI",
     store_selection_keys = "<Tab>",
   },
+  keys = {},
 }
+
+--TODO
+--
+-- add keymaps
