32dcb0051594b178b40e5caa57f68e4680cb6528
jacakira <tanase.akira@gmail.com>
2024-05-31 01:14:49 +0100

    added keybindings

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -7,10 +7,44 @@ return {
     history = false,
     enable_autosnippets = true,
     store_selection_keys = "<Tab>",
+    update_events = "TextChanged, TextChangedI",
   },
-  keys = {},
+  keys = {
+    {
+      "<tab>",
+      function()
+        return require("luasnip").expand_or_jumpable() and "<Plug>luasnip-expand-or-jump" or "<tab>"
+      end,
+      expr = true,
+      silent = true,
+      mode = "i",
+    },
+    {
+      "<tab>",
+      function()
+        return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
+      end,
+      expr = true,
+      silent = true,
+      mode = "s",
+    },
+    {
+      "<S-tab>",
+      function()
+        return require("luasnip").jumpable(-1) and "<Plug>luasnip-jump-prev" or "<S-tab>"
+      end,
+      expr = true,
+      silent = true,
+      mode = "i",
+    },
+    {
+      "<S-tab>",
+      function()
+        return require("luasnip").jumpable(-1) and "<Plug>luasnip-jump-prev" or "<S-tab>"
+      end,
+      expr = true,
+      silent = true,
+      mode = "s",
+    },
+  },
 }
-
---TODO
---
--- add keymaps
